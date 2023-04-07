d346d84714b3900824fc70c4ffaed98569181563
unknown <vladimir.vortex@gmail.com>
2023-04-07 09:26:01 +0300

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://avangraph-cloud-web-rest-api.herokuapp.com',
-  imagesUrl: 'https://avangraph-cloud-web-rest-api.herokuapp.com/static/images'
+  apiUrl: 'https://avangraph-cloud-web-rest-api.herokuapp.com/api/v1',
+  imagesUrl: 'https://avangraph-cloud-web-rest-api.herokuapp.com/static/images',
 };
